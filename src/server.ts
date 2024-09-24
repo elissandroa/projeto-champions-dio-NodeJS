9e7c726e1a589388149aca8dcfea60f54b83b691
elissandroa <elissandroa@hotmail.com>
2024-09-24 07:37:49 -0300

    Utilizando o Express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import {Request, Response} from 'express'
+
+const app = express()
+
+const port = process.env.PORT
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: "Funcionando" })
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Rodando na porta: ${port}!`)
+})
+
